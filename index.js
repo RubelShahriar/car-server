1b3978162b659d8ac75df4d3c012b9bb8172c373
RubelShahriar <mr.rubelshahriar@gmail.com>
2022-01-30 19:11:50 +0600

    upload

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const port = process.env.PORT || 4000
 app.use(cors())
 app.use(express.json())
 app.use(fileUpload())
-app.use(bodyParser.json({limit: '5mb'}))
-app.use(bodyParser.urlencoded({limit: '5mb', extended: true, parameterLimit: 100000}))
+// app.use(bodyParser.json({limit: '5mb'}))
+// app.use(bodyParser.urlencoded({limit: '5mb', extended: true, parameterLimit: 100000}))
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qudl0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
