92ae4e479871cdd255dbb1a6af1f84b6a99c906c
RubelShahriar <mr.rubelshahriar@gmail.com>
2022-01-30 18:51:13 +0600

    update

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const port = process.env.PORT || 4000
 app.use(cors())
 app.use(express.json())
 app.use(fileUpload())
+app.use(express.urlencoded({
+    extended: true,
+    limit: '5mb'
+}))
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qudl0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
